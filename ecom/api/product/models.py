15f3c6728e38380f2aa69887cc2b4b0abbd0b508
Harshalyadav <shailyadav3166@gmail.com>
2021-12-17 15:52:02 +0530

    React and Django

---
+++ ecom/api/product/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+from api.category.models import Category
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+    price =  models.CharField(max_length=50)
+    stock =  models.CharField(max_length = 50)
+    is_active = models.BooleanField(default = True, blank=True)
+    image =  models.ImageField(upload_to = 'image/', blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete = models.SET_NULL, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now=True)
+ 
+ 
+ 
+    def  __str__(self):
+        return self.name
---
+++ ecom/api/product/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from .serializers import ProductSerializer
+from .models import Product
+# Create your views here.
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all().order_by('id')
+    serializer_class = ProductSerializer
